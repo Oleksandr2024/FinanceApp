eb3a1c85524718cfd743f887cadf33864e2bc0bf
AlexandrMykhnovetski <674403324@ukr.net>
2023-07-29 14:12:21 +0300

    added real currency api to manage currency rate

--- client/src/pages/HomePage/MainInfo/HomeSidebar/CurrencyRate/currencyApi.js
+++ client/src/pages/HomePage/MainInfo/HomeSidebar/CurrencyRate/currencyApi.js
@@ -1,16 +1,16 @@
-//import axios from "axios"; also uncomment when use this API
+import axios from "axios";
 
 export const fetchCurrencyRate = async () => {
-  return 37; //hardcoded for now not to exceed the limit per month (200)
-  // try {
-  //   const response = await axios.get(process.env.REACT_APP_CURRENCY_API);
+  // return 37; hardcoded for now not to exceed the limit per month (200)
+  try {
+    const response = await axios.get(process.env.REACT_APP_CURRENCY_API);
 
-  //   // Extract the conversion rate from the response
-  //   const usdToUahRate = Number(response.data.rates.UAH).toFixed(2);
+    // Extract the conversion rate from the response
+    const usdToUahRate = Number(response.data.rates.UAH).toFixed(2);
 
-  //   return usdToUahRate;
-  // } catch (error) {
-  //   console.error("Error fetching conversion rate:", error);
-  //   return null;
-  // }
+    return usdToUahRate;
+  } catch (error) {
+    console.error("Error fetching conversion rate:", error);
+    return null;
+  }
 };
